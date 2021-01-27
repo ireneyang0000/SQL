151be6ad8243dd95de7dd596470bf6cb2042a2b7
ireneyang0000 <lima@lis-macbook-pro.myfiosgateway.com>
2021-01-26 21:16:56 -0500

    leetcode hard

--- hard_1097_Game_play_analysis.sql
+++ hard_1097_Game_play_analysis.sql
@@ -18,3 +18,12 @@ values
 (3,4,'2016-07-03',5);
 
 
+select a.day as install_dt,count(a.player_id) as installs,round((sum(case when b.games_played is not null then 1 else 0 end)/count(a.player_id)),2)  as Day1_retention
+from
+(select distinct player_id, min(event_date) as day
+    from activity
+    group by 1) a
+left join activity b
+on a.player_id = b.player_id
+and a.day = b.event_date - 1
+group by 1
